75c5bbec1775fc1cbbcc5b815aa66feeed023ac6
the-movie-phimmoi <daiphu094@gmail.com>
2025-01-25 03:43:01 +0700

    cap nhat

--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import menu from '../assets/menu.png'
+import menu99 from '../assets/menu99.jpg'
 import menu7 from '../assets/menu7.png'
 import menu3 from '../assets/menu3.png'
 
 
 const Menu = () => {
-    return (
-        <div>
-            <img src={menu} alt="" />
-            <div>
-                <img src={menu7} alt="" />
-                <img src={menu3} alt="" />
-            </div>
-        </div>
-    )
+  return (
+    <div>
+        <img src={menu99} alt="" />
+        <img src={menu7} alt="" />
+        <img src={menu3} alt="" />
+        
+    </div>
+  )
 }
 
 export default Menu
