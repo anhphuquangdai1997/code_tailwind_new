770337bb7061448718a7b402c3772f538cfb97bd
the-movie-phimmoi <daiphu094@gmail.com>
2025-02-01 14:49:07 +0700

    cap nhat

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Giaodich from './component/Giaodich';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './component/Home';
-import ChuyenTien from './component/Chuyentien';
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <Routes>
       <Route path="/" element={<Home />} />
         <Route path="/giaodich" element={<Giaodich />} />
-        <Route path="/chuyentien" element={<ChuyenTien />} />
       </Routes>
     </Router>
     </>
